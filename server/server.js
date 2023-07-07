bfdefb8ebb7031642c29a85e2866c6622e2989d5
drili <DriliB@hotmail.com>
2023-07-07 15:03:08 +0200

    :hammer: init project + serverjs

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://dbkynetic:Kynetic123123@cluster0.f80a2n8.mongodb.net/';
+mongoose.connect(uri, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("::: MongoDB database connection established successfully");
+});
+
+app.listen(5000, () => {
+    console.log("::: Server is running on port 5000");
+});
