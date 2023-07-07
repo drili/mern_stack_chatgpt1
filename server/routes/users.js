4062564d6fabf04cfc0b3ed900dbe695258b6f2b
drili <DriliB@hotmail.com>
2023-07-07 15:13:02 +0200

    :hammer: routes users - register

---
+++ server/routes/users.js
@@ -0,0 +1,49 @@
+const router = require("express").Router()
+let User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+// ***
+// * Register Route
+// ***
+router.route("/register").post((req, res) => {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        return res.status(400).json({ msg: "::: Please enter all fields" })
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ msg: "::: User already exists" })
+            }
+
+            const newUser = new User({
+                username,
+                password
+            })
+
+            newUser,save()
+                .then(user => {
+                    jwt.sign(
+                        { id: user.id },
+                        "my_jwt_secret",
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if (err) {
+                                throw err
+                            }
+
+                            res.json({
+                                token,
+                                user: {
+                                    id: user.id,
+                                    username: user.username
+                                }
+                            })
+                        }
+                    )
+                })
+        })
+})
