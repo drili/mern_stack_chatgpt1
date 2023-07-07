ac10da448e84deb52c47cbc9ca0eb984701b1aca
drili <DriliB@hotmail.com>
2023-07-07 15:17:33 +0200

    wip

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 
-const app = express();
+// Routes
+const userRouter = require("./routes/users")
 
-app.use(cors());
-app.use(express.json());
+const app = express()
 
-const uri = 'mongodb+srv://dbkynetic:Kynetic123123@cluster0.f80a2n8.mongodb.net/';
+app.use(cors())
+app.use(express.json())
+app.use("/users", userRouter)
+
+const uri = 'mongodb+srv://dbkynetic:Kynetic123123@cluster0.f80a2n8.mongodb.net/'
 mongoose.connect(uri, { 
     useNewUrlParser: true,
     useUnifiedTopology: true 
-});
+})
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 connection.once('open', () => {
-    console.log("::: MongoDB database connection established successfully");
-});
+    console.log("::: MongoDB database connection established successfully")
+})
 
 app.listen(5000, () => {
-    console.log("::: Server is running on port 5000");
-});
+    console.log("::: Server is running on port 5000")
+})
