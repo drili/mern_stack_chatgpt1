3b4c70314cc0ac3ba828d2b4764f7c32de73521b
drili <DriliB@hotmail.com>
2023-07-07 15:16:29 +0200

    :hammer: routes users - login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -47,3 +47,43 @@ router.route("/register").post((req, res) => {
                 })
         })
 })
+
+// ***
+// * Login Route
+// ***
+router.route("/login").post((req, res) => {
+    const { username, password } = req.body
+
+    if(!username || !password) {
+        return res.status(400).json({ msg: '::: Please enter all fields' });
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (!user) return res.status(400).json({ msg: '::: User does not exist' });
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (!isMatch) return res.status(400).json({ msg: '::: Invalid credentials' });
+
+                    jwt.sign(
+                        { id: user.id },
+                        'my_jwt_secret',
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if (err) throw err;
+
+                            res.json({
+                                token,
+                                user: {
+                                    id: user.id,
+                                    username: user.username,
+                                }
+                            });
+                        }
+                    )
+                });
+        })
+})
+
+module.exports = router
