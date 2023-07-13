4e824d8bd492a9f2a35d6ec5e4b6e062aaeb03d9
drili <DriliB@hotmail.com>
2023-07-13 14:33:02 +0200

    :hammer: moved componets to components dir

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,7 +37,10 @@ const Navbar = () => {
                         <span className='flex flex-col justify-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
                         <p>{user && user.email}</p>
                         </span>
-                        <button onClick={handleLogout} type="button" className="text-white bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Logout</button>
+                        <button 
+                            onClick={handleLogout} 
+                            type="button" 
+                            className="text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-medium rounded-md text-sm px-4 py-2 text-center mr-3 md:mr-0">Logout</button>
                         
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
