2cc3b3fee58b35d86c387e87568f3481f562cfbd
drili <DriliB@hotmail.com>
2023-09-11 14:34:40 +0200

    :construction: TODO fix

--- server/models/TimeRegistration.js
+++ server/models/TimeRegistration.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 
+// TODO: Fix this, move to router part of "/register-time"
 const date = new Date();
 const day = String(date.getDate()).padStart(2, '0');
 const month = String(date.getMonth() + 1).padStart(2, '0'); 
