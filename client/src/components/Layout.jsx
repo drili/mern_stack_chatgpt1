645bd2006fa4ddf5d7fdd67775f59ee4555affea
drili <DriliB@hotmail.com>
2023-09-22 14:00:41 +0200

    wip sidebar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
 import { AiOutlineMenu } from "react-icons/ai"
+import { BiSolidDashboard } from "react-icons/bi"
+import { GiSprint } from "react-icons/gi"
+import { TbClockCheck } from "react-icons/tb"
+import { PiUsersFill } from "react-icons/pi"
+import { MdAdminPanelSettings } from "react-icons/md"
+import { Link } from 'react-router-dom';
 
 const Layout = ({ children }) => {
     const [showSidebar, setShowSidebar] = useState(true)
@@ -15,7 +21,31 @@ const Layout = ({ children }) => {
             {showSidebar && (
                 <aside className='relative bg-indigo-50 w-1/6 min-h-screen p-4'>
                     <div className='sidebar-content fixed bottom-20 left-0 p-4'>
-                        <h2>Sidebar Content</h2>
+                        <span>
+                            <h2>Sidebar Content</h2>
+                        </span>
+
+                        <span className='sidebarLinks'>
+                            <Link to="/dashboard" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
+                                <BiSolidDashboard/>Dashboard
+                            </Link>
+
+                            <Link to="/" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
+                                <GiSprint/>Sprint Overview
+                            </Link>
+
+                            <Link to="/" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
+                                <TbClockCheck/> Time Registrations
+                            </Link>
+
+                            <Link to="/" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
+                                <PiUsersFill/> Customers
+                            </Link>
+
+                            <Link to="/" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
+                               <MdAdminPanelSettings/> Admin
+                            </Link>
+                        </span>
                     </div>
                 </aside>
             )}
