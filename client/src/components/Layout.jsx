6f5c22dfa1d4bc2a0acc5da1a6252398a0543b95
drili <DriliB@hotmail.com>
2023-09-22 13:47:02 +0200

    wip sidebar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
+import { AiOutlineMenu } from "react-icons/ai"
 
 const Layout = ({ children }) => {
     const [showSidebar, setShowSidebar] = useState(true)
+    const [isMobile, setIsMobile] = useState(false)
 
     const toggleSidebar = () => {
         setShowSidebar(!showSidebar)
@@ -11,20 +13,24 @@ const Layout = ({ children }) => {
     return (
         <div className='layout flex'>
             {showSidebar && (
-                <aside className='bg-gray-200 w-1/5 min-h-screen p-4'>
-                    <h2>Sidebar</h2>
+                <aside className='relative bg-indigo-50 w-1/6 min-h-screen p-4'>
+                    <div className='sidebar-content fixed bottom-20 left-0 p-4'>
+                        <h2>Sidebar Content</h2>
+                    </div>
                 </aside>
             )}
 
             <main className='w-full min-h-screen p-4'>
                 <Navbar />
 
-                <button 
-                    className='fixed bottom-4 left-4 bg-blue-500 text-white px-3 py-1 rounded-md z-10'
-                    onClick={toggleSidebar}
-                >
-                    {showSidebar ? "Hide sidebar" : "Show sidebar"}
+                {isMobile && (
+                    <button 
+                        className='fixed bottom-4 left-4 bg-indigo-500 text-white px-3 py-1 rounded-md z-10'
+                        onClick={toggleSidebar}
+                    >
+                    {showSidebar && isMobile ? <AiOutlineMenu/> : <AiOutlineMenu/>}
                 </button>
+                )}
 
                 <section id="mainSection" className='max-w-screen-xl flex mx-auto p-4"'>
                     <div className='w-full'>
