6d0f3c4d80998fbb1ae732dd200f0c4ddd38055e
drili <DriliB@hotmail.com>
2023-09-22 11:07:50 +0200

    wip sidebar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
-    return (
-        <div className='layout'>
-            <Navbar />
+    const [showSidebar, setShowSidebar] = useState(true)
 
-            <section id="mainSection" className='max-w-screen-xl flex mx-auto p-4"'>
-                <div className='w-full'>
-                    {children}
-                </div>
-            </section>
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar)
+    }
+
+    return (
+        <div className='layout flex'>
+            {showSidebar && (
+                <aside className='bg-gray-200 w-1/5 min-h-screen p-4'>
+                    <h2>Sidebar</h2>
+                </aside>
+            )}
+
+            <main className='w-full min-h-screen p-4'>
+                <Navbar />
+
+                <button 
+                    className='fixed bottom-4 left-4 bg-blue-500 text-white px-3 py-1 rounded-md z-10'
+                    onClick={toggleSidebar}
+                >
+                    {showSidebar ? "Hide sidebar" : "Show sidebar"}
+                </button>
+
+                <section id="mainSection" className='max-w-screen-xl flex mx-auto p-4"'>
+                    <div className='w-full'>
+                        {children}
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
