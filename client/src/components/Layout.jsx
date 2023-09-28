c65edf4d9fa29587e7478e12b95f6b38288c2029
drili <DriliB@hotmail.com>
2023-09-28 14:07:45 +0200

    wip layout

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
-import { Link } from 'react-router-dom';
-import {  } from "react-icons/bs";
+import { Link, useLocation } from 'react-router-dom';
+import { BsHouseDoor, BsList, BsCalendar, BsClock, BsPeople, BsPerson, BsGear } from 'react-icons/bs';
+import SidebarLink from './navbar/SidebarLink';
 
 const Layout = ({ children }) => {
     const [showSidebar, setShowSidebar] = useState(true)
     const [isMobile, setIsMobile] = useState(false)
+
+    const location = useLocation()
+    const currentPath = location.pathname
 
     const toggleSidebar = () => {
         setShowSidebar(!showSidebar)
@@ -22,32 +26,67 @@ const Layout = ({ children }) => {
                         </span>
 
                         <span className='sidebarLinks'>
-                            <Link to="/dashboard" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                                Dashboard
+                            <SidebarLink
+                                menuLink="/dashboard"
+                                linkText="Dashboard"
+                                currentPath={currentPath}
+                                iconComponent={BsHouseDoor}
+                            />
+
+                            <Link 
+                                to="/dashboard" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/dashboard' ? 'bg-indigo-100' : '200:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                                <BsHouseDoor/>Dashboard
                             </Link>
 
-                            <Link to="/workflow" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                                Workflow
+                            <Link 
+                                to="/workflow" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/workflow' ? 'bg-indigo-200' : 'hover:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                                <BsList/>Workflow
                             </Link>
 
-                            <Link to="#" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                                Sprint Overview
+                            <Link 
+                                to="#" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/sprint-overview' ? 'bg-indigo-200' : 'hover:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                                <BsCalendar/>Sprint Overview
                             </Link>
 
-                            <Link to="#" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                                Time Registrations
+                            <Link 
+                                to="#" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/time-registration' ? 'bg-indigo-200' : 'hover:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                                <BsClock/>Time Registrations
                             </Link>
 
-                            <Link to="/customers" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                                Customers
+                            <Link 
+                                to="/customers" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/customers' ? 'bg-indigo-200' : 'hover:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                                <BsPeople/>Customers
                             </Link>
 
-                            <Link to="/profile" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                               Profile
+                            <Link 
+                                to="/profile" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/profile' ? 'bg-indigo-200' : 'hover:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                               <BsPerson/>Profile
                             </Link>
 
-                            <Link to="#" className='flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                               Admin
+                            <Link 
+                                to="#" 
+                                className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${
+                                    currentPath === '/admin' ? 'bg-indigo-200' : 'hover:bg-gray-100'
+                                  } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                               <BsGear/>Admin
                             </Link>
                         </span>
                     </div>
