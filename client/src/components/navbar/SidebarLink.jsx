2733fce49553f768568aab967ffafa0e419a5bde
drili <DriliB@hotmail.com>
2023-09-28 14:09:21 +0200

    wip

--- client/src/components/navbar/SidebarLink.jsx
+++ client/src/components/navbar/SidebarLink.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 const SidebarLink = ({ menuLink, linkText, currentPath, iconComponent }) => {
     return (
         <Link
-            // TODO: fix this error: (menu link goes to the wrong link)
-            to={`/${menuLink}`}
+            to={menuLink}
             className={`flex items-center gap-4 py-2 pl-3 pr-4 text-gray-900 rounded ${currentPath === menuLink ? 'bg-indigo-100' : '200:bg-gray-100'
                 } md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
             {React.createElement(iconComponent)}{linkText}
