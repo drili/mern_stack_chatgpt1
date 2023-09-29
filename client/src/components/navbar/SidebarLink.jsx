c1e1a21a2ba21a8bd85d2fb136c3bc7be8ff1db8
drili <DriliB@hotmail.com>
2023-09-29 12:08:49 +0200

    wip sidebar

--- client/src/components/navbar/SidebarLink.jsx
+++ client/src/components/navbar/SidebarLink.jsx
@@ -5,8 +5,8 @@ const SidebarLink = ({ menuLink, linkText, currentPath, iconComponent }) => {
     return (
         <Link
             to={menuLink}
-            className={`tracking-wide gap-4 flex items-center py-2 px-4 text-gray-900 rounded 
-                ${currentPath === menuLink ? 'bg-indigo-100 font-bold' : 'font-normal'} dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+            className={`tracking-wide gap-4 flex items-center py-2 px-4 text-slate-500 rounded text-sm 
+                ${currentPath === menuLink ? 'bg-indigo-200 font-bold' : 'font-normal'} dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
             {React.createElement(iconComponent)}{linkText}
         </Link>
     )
