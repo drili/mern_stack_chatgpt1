b50db6cc62b07ff1d58dea3986607de9868cffea
drili <DriliB@hotmail.com>
2023-10-19 11:10:10 +0200

    :art: sprintoverview wip

--- client/src/components/sprintoverview/Accordion.jsx
+++ client/src/components/sprintoverview/Accordion.jsx
@@ -86,6 +86,53 @@ const DefaultAccordion = ({ content, taskId }) => {
                                     <Table.Cell>
                                         0
                                     </Table.Cell>
+                                    <Table.Cell>
+                                        <a
+                                        className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                                        href="/tables"
+                                        >
+                                        <p>
+                                            Edit Task
+                                        </p>
+                                        </a>
+                                    </Table.Cell>
+                                </Table.Row>
+
+                                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                        Apple MacBook Pro 17"
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        Sliver
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        Laptop
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        Horse
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        $2999
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        510
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        $2999
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        0
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <a
+                                        className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                                        href="/tables"
+                                        >
+                                        <p>
+                                            Edit Task
+                                        </p>
+                                        </a>
+                                    </Table.Cell>
                                 </Table.Row>
                             </Table.Body>
                         </Table>
