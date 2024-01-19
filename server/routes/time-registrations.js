887b61c8201f44c994c265922f56dfe1c8f68358
drili <DriliB@hotmail.com>
2024-01-19 12:56:39 +0100

    :memo: todo

--- server/routes/time-registrations.js
+++ server/routes/time-registrations.js
@@ -178,9 +178,6 @@ router.route("/register-time").post(async (req,res) => {
             taskId,
             timeRegistered,
             description,
-            // TODO: Add the sprintId based on the "currentTime" value.
-            // Example: If user registeres time registrations in 11-12-2023 (december), but on a November sprint task, the sprintId so be december, not november.
-            // ....
             sprintId: newSprintId,
             currentTime: formattedDate,
             registrationType: registrationTypeValue,
