4cbd893c57c5683e589384d1f091db87f5e047b6
drili <DriliB@hotmail.com>
2024-01-19 13:51:52 +0100

    :lipstick: responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,8 +16,13 @@ const Navbar = () => {
     const navigate = useNavigate()
     const [activeYear, setActiveYear] = useState("")
     const [sprintYears, setSprintYears] = useState([])
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
     const { user, setUser } = useContext(UserContext)
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen)
+    }
 
     const handleSprintYearChange = async (e) => {
         const newActiveYear = e
@@ -79,6 +84,50 @@ const Navbar = () => {
         // }
     }, [user])
 
+    const menuItems = (
+        <>
+            <span id="navbarButtons" className='flex gap-4 mt-10 flex-col w-full md:flex-row md:mr-4 md:items-center md:mt-0'>
+                <div>
+                    <select
+                        className={`${inputClasses} min-w-[100px] bg-white`}
+                        defaultValue=""
+                        onChange={(e) => handleSprintYearChange(e.target.value)}
+                    >
+                        <option value={activeYear}>{activeYear}</option>
+                        {sprintYears
+                            .filter((year) => year.sprintYear !== activeYear)
+                            .map((year) => (
+                                <option key={year._id} value={year.sprintYear}>{year.sprintYear}</option>
+                            ))}
+                    </select>
+                </div>
+
+                <Link to="/register-offtime">
+                    <button type="submit" className='h-fit whitespace-nowrap button text-black bg-white border-rose-500 hover:bg-rose-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-violet-800'>Register Off- & Sicktime</button>
+                </Link>
+
+                <Link to="/create-task">
+                    <button type="submit" className='bg-rose-500 h-fit whitespace-nowrap button text-white border-rose-500 hover:bg-rose-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-violet-800'>Create Task</button>
+                </Link>
+            </span>
+
+            <span className='flex flex-row align-center items-center justify-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-5 md:mt-0'>
+                <p className='font-bold'>@{username}</p>
+                <img
+                    className='w-[40px] h-[40px] rounded-full object-cover ml-2'
+                    src={`${imageSrc}${userImg}`} /
+                >
+            </span>
+
+            <span className='flex items-center'>
+                <button
+                    onClick={handleLogout}
+                    type="button"
+                    className="h-fit text-black font-medium rounded-md text-sm px-4 mt-5 py-2 text-center mr-3 md:mr-0">Logout</button>
+            </span>
+        </>
+    )
+
     return (
         <>
             <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
@@ -87,58 +136,23 @@ const Navbar = () => {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Unify</span>
                     </a>
 
-                    <div className="flex md:order-2">
-                        <span id="navbarButtons" className='flex gap-4 items-center mr-4'>
-                            <div>
-                                <select
-                                    className={`${inputClasses} min-w-[100px] bg-white`}
-                                    defaultValue=""
-                                    onChange={(e) => handleSprintYearChange(e.target.value)}
-                                >
-                                    <option value={activeYear}>{activeYear}</option>
-                                    {sprintYears
-                                        .filter((year) => year.sprintYear !== activeYear)
-                                        .map((year) => (
-                                        <option key={year._id} value={year.sprintYear}>{year.sprintYear}</option>
-                                    ))}
-                                </select>
-                            </div>
+                    <button
+                        onClick={toggleMobileMenu}
+                        data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded={isMobileMenuOpen}>
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
 
-                            <Link to="/register-offtime">
-                                <button type="submit" className='h-fit whitespace-nowrap button text-black bg-white border-rose-500 hover:bg-rose-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-violet-800'>Register Off- & Sicktime</button>
-                            </Link>
-
-                            <Link to="/create-task">
-                                <button type="submit" className='bg-rose-500 h-fit whitespace-nowrap button text-white border-rose-500 hover:bg-rose-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-violet-800'>Create Task</button>
-                            </Link>
-                        </span>
-
-                        <span className='flex flex-row align-center items-center justify-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                            <p className='font-bold'>@{username}</p>
-                            <img
-                                className='w-[40px] h-[40px] rounded-full object-cover ml-2'
-                                src={`${imageSrc}${userImg}`} /
-                            >
-                        </span>
-
-                        <span className='flex items-center'>
-                            <button
-                                onClick={handleLogout}
-                                type="button"
-                                className="h-fit text-black font-medium rounded-md text-sm px-4 py-2 text-center mr-3 md:mr-0">Logout</button>
-
-                            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-                                <span className="sr-only">Open main menu</span>
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                                </svg>
-                            </button>
-                        </span>
+                    <div className="hidden md:flex md:order-2">
+                        {menuItems} 
                     </div>
 
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-
+                    <div className={`${isMobileMenuOpen ? 'flex flex-col items-end' : 'hidden'} md:hidden w-full`} id="navbar-sticky">
+                        {menuItems}
                     </div>
+
                 </div>
             </nav>
 
