2bcc66d62a232dc429b39df412d59936b3c48711
drili <DriliB@hotmail.com>
2024-02-02 09:25:08 +0100

    :memo:

--- client/src/components/task/TaskChat.jsx
+++ client/src/components/task/TaskChat.jsx
@@ -21,7 +21,7 @@ const TaskChat = ({ taskID }) => {
     return (
         <div className="flex flex-col h-full bg-white">
             <div className="flex flex-col overflow-y-auto max-h-[55vh]">
-                {/* TODO: Include message formatting, e.g. bold, mentions, etc. */}
+                {/* TODO: Include message formatting, e.g. bold, mentions, etc... */}
                 {messages.map((message, index) => (
                     <div key={index} className="mb-4 flex align-top">
                         <div>
