885cabed8f4d5ac813c6c5fb6e5007807db52eab
drili <DriliB@hotmail.com>
2024-02-06 11:15:18 +0100

    :hammer: task settings alloc

--- client/src/components/task/TaskModalSettings.jsx
+++ client/src/components/task/TaskModalSettings.jsx
@@ -280,20 +280,22 @@ const TaskModalSettings = ({ labelClasses, inputClasses, taskID, fetchTaskData, 
                                                         <input type="hidden" name='taskPersonId' value={user.user._id}  />
 
                                                         {taskType !== "quickTask" && (
-                                                            <span className='flex items-center gap-2 mr-2'>
-                                                                <input
-                                                                    className="max-w-[100px] px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-0 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-violet-500"
-                                                                    type="number"
-                                                                    max="100"
-                                                                    min="1"
-                                                                    value={percentageValues[user.user._id] || ''}
-                                                                    onChange={(e) => handlePercentageChange(user.user._id, e.target.value)}
-                                                                    name="personPercentage"
-                                                                    />
-                                                                <label className='text-xs font-normal whitespace-nowrap' htmlFor="personPercentage">% alloc</label>
-                                                            </span>
+                                                            <>
+                                                                <span className='flex items-center gap-2 mr-2'>
+                                                                    <input
+                                                                        className="max-w-[100px] px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-0 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-violet-500"
+                                                                        type="number"
+                                                                        max="100"
+                                                                        min="1"
+                                                                        value={percentageValues[user.user._id] || ''}
+                                                                        onChange={(e) => handlePercentageChange(user.user._id, e.target.value)}
+                                                                        name="personPercentage"
+                                                                        />
+                                                                    <label className='text-xs font-normal whitespace-nowrap' htmlFor="personPercentage">% alloc</label>
+                                                                </span>
+                                                                <button type="submit" className='border-rose-500 px-2 py-0 text-sm'>Update</button>
+                                                            </>
                                                         )}
-                                                        <button type="submit" className='border-rose-500 px-2 py-0 text-sm'>Update</button>
 
                                                     </form>
                                                 </>
