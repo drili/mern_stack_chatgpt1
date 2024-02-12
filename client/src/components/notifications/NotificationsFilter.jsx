7670d161bbd145ae651488dd2d6b175963c87a9f
drili <DriliB@hotmail.com>
2024-02-12 16:02:52 +0100

    :construction: wip notifications page

---
+++ client/src/components/notifications/NotificationsFilter.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { BsSearch, BsCalendarFill } from "react-icons/bs"
+
+const inputClasses = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-violet-500"
+const labelClasses = "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+
+const NotificationsFilter = () => {
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const handleSearchTerm = async (e) => {
+        const newSearchTerm = e.target.value
+        setSearchTerm(newSearchTerm)
+    }
+
+    return (
+        <div id='NotificationsFilter' className='py-4 px-5 border-0 rounded-lg bg-slate-50 relative flex flex-col w-full outline-none focus:outline-none mb-10'>
+            <section className='flex justify-end gap-8 flex-col md:flex-row'>
+
+                <div id='WorkflowFilters-searchField'>
+                    <span className='flex gap-2 items-center'>
+                        <input
+                            type="text"
+                            className={`${inputClasses} min-w-[200px]`}
+                            placeholder='Search notification(s)'
+                            onChange={handleSearchTerm}
+                        />
+                        <BsSearch size={20}></BsSearch>
+                    </span>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+
+export default NotificationsFilter
--- client/src/pages/Notifications.jsx
+++ client/src/pages/Notifications.jsx
@@ -1,14 +1,79 @@
 import React from 'react'
+
 import PageHeading from '../components/PageHeading'
+import NotificationsFilter from '../components/notifications/NotificationsFilter'
+import userImageDefault from "../assets/profile-pics/default-image.jpg"
 
 const Notifications = () => {
     return (
         <div id='NotificationsPage'>
-            <PageHeading 
+            <PageHeading
                 heading="Your Notifications"
                 subHeading={`Navigate through your different notifications`}
                 suffix=""
             />
+
+            <NotificationsFilter />
+
+            <div className='grid grid-cols-3 gap-10'>
+                <section id='NotificationsSections' className='border rounded-md flex flex-col gap-2 col-span-2'>
+                    <div className='flex gap-5 p-5 hover:cursor-pointer hover:bg-slate-50'>
+                        <span className='flex'>
+                            <img
+                                src={`${userImageDefault}`}
+                                className="min-w-[50px] h-[50px] object-cover rounded-md"
+                            />
+                        </span>
+
+                        <span className='flex flex-col overflow-hidden'>
+                            <h3 className='font-bold'>Jakob Bøgh Rasmussen</h3>
+                            <span className='flex justify-between'>
+                                <p className='text-sm text-slate-500 mb-2'>Mentioned you in task "taskName"</p>
+                                <p className='text-sm font-bold text-slate-900 mb-2'>16:00 12-02-2024</p>
+                            </span>
+                            <p className='trunateCustom'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto nemo dignissimos quas voluptatibus maxime veritatis esse soluta incidunt enim exercitationem laboriosam deserunt officia ut, ad neque sunt, earum quibusdam optio!</p>
+                        </span>
+                    </div>
+                    <div className='flex gap-5 p-5 hover:cursor-pointer hover:bg-slate-50'>
+                        <span className='flex'>
+                            <img
+                                src={`${userImageDefault}`}
+                                className="min-w-[50px] h-[50px] object-cover rounded-md"
+                            />
+                        </span>
+
+                        <span className='flex flex-col overflow-hidden'>
+                            <h3 className='font-bold'>Jakob Bøgh Rasmussen</h3>
+                            <span className='flex justify-between'>
+                                <p className='text-sm text-slate-500 mb-2'>Mentioned you in task "taskName"</p>
+                                <p className='text-sm font-bold text-slate-900 mb-2'>16:00 12-02-2024</p>
+                            </span>
+                            <p className='trunateCustom'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto nemo dignissimos quas voluptatibus maxime veritatis esse soluta incidunt enim exercitationem laboriosam deserunt officia ut, ad neque sunt, earum quibusdam optio!</p>
+                        </span>
+                    </div>
+                    <div className='flex gap-5 p-5 hover:cursor-pointer hover:bg-slate-50'>
+                        <span className='flex'>
+                            <img
+                                src={`${userImageDefault}`}
+                                className="min-w-[50px] h-[50px] object-cover rounded-md"
+                            />
+                        </span>
+
+                        <span className='flex flex-col overflow-hidden'>
+                            <h3 className='font-bold'>Jakob Bøgh Rasmussen</h3>
+                            <span className='flex justify-between'>
+                                <p className='text-sm text-slate-500 mb-2'>Mentioned you in task "taskName"</p>
+                                <p className='text-sm font-bold text-slate-900 mb-2'>16:00 12-02-2024</p>
+                            </span>
+                            <p className='trunateCustom'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto nemo dignissimos quas voluptatibus maxime veritatis esse soluta incidunt enim exercitationem laboriosam deserunt officia ut, ad neque sunt, earum quibusdam optio!</p>
+                        </span>
+                    </div>
+                </section>
+
+                <section id='NotificationsTasks' className='col-span-1'>
+                    <h2 className='font-bold mb-5'>Tasks you have recently been added to</h2>
+                </section>
+            </div>
         </div>
     )
 }
